4cd1b0ca3d8975324388b415f3883b4334ec9244
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-31 10:23:39 +0200

    Magnitude

--- read_data.m
+++ read_data.m
@@ -6,3 +6,12 @@ dataMv = (data./(2^16)-1/2).*3;
 fftData= fft(dataMv);
 
 %%Rectify data: Outliers weg mooie schaal, bereik mooi aangepast aan data
+N = 8700;
+df=1000/N; %frequency resolution
+sampleIndex = -N/2:N/2-1; %ordered index for FFT plot
+f=sampleIndex*df; %x-axis index converted to ordered frequencies
+stem(f,abs(fftData)); %magnitudes vs frequencies
+xlabel('f (Hz)'); ylabel('|X(k)|');
+
+phase=atan2(imag(X),real(X))*180/pi; %phase information
+plot(f,phase); %phase vs frequencie
