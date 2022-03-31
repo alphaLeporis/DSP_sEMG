ab56aabe42da9106bf4ab43128d7e7dd6b1d1832
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-31 10:17:47 +0200

    FFT

--- read_data.m
+++ read_data.m
@@ -2,3 +2,7 @@
 [data, t, header] = FileParser("./PP01/S1_MVC_delt_links.txt");
 
 dataMv = (data./(2^16)-1/2).*3;
+
+fftData= fft(dataMv);
+
+%%Rectify data: Outliers weg mooie schaal, bereik mooi aangepast aan data
