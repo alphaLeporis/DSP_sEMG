0ea90ea7b71dc3cce883ebc68509a59fd01b2e57
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-03-31 10:03:40 +0200

    Mv

--- read_data.m
+++ read_data.m
@@ -1,1 +1,4 @@
-[data, t, header] = FileParser("./PP01/S1_MVC_delt_links.txt")
+%Reading data using fileparser
+[data, t, header] = FileParser("./PP01/S1_MVC_delt_links.txt");
+
+dataMv = (data./(2^16)-1/2).*3;
