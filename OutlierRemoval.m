d759195b479f7aa1ff060c7beb4f6f0a9fcb9206
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-05-05 10:03:10 +0200

    Update OutlierRemoval.m

--- OutlierRemoval.m
+++ OutlierRemoval.m
@@ -5,6 +5,9 @@ standardDeviation = std(data);
 meanDat = mean(data);
 toRemove = (data < (meanDat-order*standardDeviation)) | (data > (meanDat+order*standardDeviation));
 indicesToRemove = find(toRemove);
-for i = 2:size(indicesToRemove)-1
-    data(indicesToRemove(i)) = (data(indicesToRemove(i) - 1) + data(indicesToRemove(i) + 1))/2;
+clms = size(data, 1);
+for i = 1:size(indicesToRemove)
+    if ~(mod(indicesToRemove(i),clms) == 0 || mod(indicesToRemove(i)-1,clms) == 0)
+        data(indicesToRemove(i)) = (data(indicesToRemove(i) - 1) + data(indicesToRemove(i) + 1))/2;
+    end
 end
