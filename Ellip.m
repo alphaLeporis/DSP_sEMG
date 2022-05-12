f745068daae269aaf83d1ff7d44ec9087d14719a
amichielsen <83591375+amichielsen@users.noreply.github.com>
2022-05-12 13:37:22 +0200

    Kutfilters

--- Ellip.m
+++ Ellip.m
@@ -1,6 +1,6 @@
 function [output] = Ellip(data, low, high, order)
 %Elliptical filter
-[b, a] = ellip(order, 3, [low, high], "bandpass");
+[b, a] = ellip(order, 3, 50, [low, high], "bandpass");
 output = filter(b, a, data);
 end
 
