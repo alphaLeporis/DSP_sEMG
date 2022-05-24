2af7fe9fb5a2c6903c0a555b9ac8a73ec9d37c8f
Alpha Leporis <louis.delooze@gmail.com>
2022-05-24 14:47:07 +0200

    Opkuisen

--- Outlier_Removal.m
+++
@@ -1,8 +0,0 @@
-[data, t, header] = FileParser("./PP01/S1_MVC_delt_links.txt");
-
-dataNoOutliers = rmoutliers(data);
-
-standardDeviation = std(data);
-%%mean = mean(data);
-
-noOutlier = OutlierRemoval(data(:, 3), 2)
